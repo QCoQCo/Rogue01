81e7e447970b4f19345c8db738bc8c9821635963
QCoQCo <kbw7700@gmail.com>
2025-07-21 21:44:51 +0900

    working on movement

--- src/main/java/com/rogue01/entity/Player.java
+++ src/main/java/com/rogue01/entity/Player.java
@@ -13,16 +13,16 @@ public class Player extends Entity {
     @Override
     public void update() {
         // 입력 처리 및 이동 로직
-        if (inputHandler.isKeyPressed('w')) {
+        if (inputHandler.isKeyPressed('w') || inputHandler.isKeyPressed(java.awt.event.KeyEvent.VK_UP)) {
             y--;
         }
-        if (inputHandler.isKeyPressed('s')) {
+        if (inputHandler.isKeyPressed('s') || inputHandler.isKeyPressed(java.awt.event.KeyEvent.VK_DOWN)) {
             y++;
         }
-        if (inputHandler.isKeyPressed('a')) {
+        if (inputHandler.isKeyPressed('a') || inputHandler.isKeyPressed(java.awt.event.KeyEvent.VK_LEFT)) {
             x--;
         }
-        if (inputHandler.isKeyPressed('d')) {
+        if (inputHandler.isKeyPressed('d') || inputHandler.isKeyPressed(java.awt.event.KeyEvent.VK_RIGHT)) {
             x++;
         }
     }
